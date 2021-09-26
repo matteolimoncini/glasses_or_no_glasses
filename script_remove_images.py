b1e622aedd58343f83eb63e1cecd8a5bcb917f21
Matteo Limoncini <44336110+matteolimoncini@users.noreply.github.com>
2021-09-26 12:36:56 +0200

    added comments

--- script_remove_images.py
+++ script_remove_images.py
@@ -1,5 +1,8 @@
+#support script used for creating a shell command to remove images not correct
+
 import os
 
+#put in a directory the cleaned dataset
 for i in os.walk("allimages/"):
     lista_immagini = i[2]
 
@@ -8,11 +11,13 @@ for i in range(1, 4501):
     name = 'face-'+str(i)+'.png'
     placeholder_lista.append(name)
 
+#detect which images are in the original dataset but not in the cleaned
 def Diff(li1, li2):
     return list(set(li1) - set(li2)) + list(set(li2) - set(li1))
 
 output_lista = Diff(lista_immagini, placeholder_lista)
 
+#write into a file shell command to remove images that are in the original dataset but not in the cleaned
 f = open('file_to_remove.txt', 'w')
 
 for i in output_lista:
